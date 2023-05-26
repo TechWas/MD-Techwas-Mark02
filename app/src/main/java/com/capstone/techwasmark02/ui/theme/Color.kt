c0ebd10cae173bb1350ead0c5c06019a055d3991
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-26 23:00:52 +0700

    add some colors

--- app/src/main/java/com/capstone/techwasmark02/ui/theme/Color.kt
+++ app/src/main/java/com/capstone/techwasmark02/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package com.capstone.techwasmark02.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Green35 = Color(0xff5bb915)
+val Green77 = Color(0xFF54B800)
 
 val Black20 = Color(0xff323232)
 val Black12 = Color(0xff1F1F1F)
@@ -10,3 +11,5 @@ val Black12 = Color(0xff1F1F1F)
 val Lime56 = Color(0xff9ED54A)
 
 val Mist97 = Color(0xffF1EEFF)
+
+val Yellow77 = Color(0xFFFFDF5D)
