1315707a23edf0ff04f92e8d6b9a083186dd5b2d
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-06-01 23:27:12 +0700

    modif: make new component

--- app/src/main/java/com/capstone/techwasmark02/ui/component/ProfileBox.kt
+++ app/src/main/java/com/capstone/techwasmark02/ui/component/ProfileBox.kt
@@ -1,2 +1,90 @@
 package com.capstone.techwasmark02.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.capstone.techwasmark02.R
+import com.capstone.techwasmark02.ui.theme.TechwasMark02Theme
+
+@Composable
+fun ProfileBox(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .clip(MaterialTheme.shapes.large)
+            .background(MaterialTheme.colorScheme.tertiary)
+            .padding(horizontal = 24.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(id = R.drawable.ic_language),
+                    contentDescription = "Language",
+                    tint = MaterialTheme.colorScheme.onTertiary
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Language",
+                    style = MaterialTheme.typography.labelMedium
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(id = R.drawable.ic_logout),
+                    contentDescription = "Logout",
+                    tint = Color.Red
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Logout",
+                    style = MaterialTheme.typography.labelMedium,
+                    color = Color.Red
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun ProfileBoxPreview() {
+    TechwasMark02Theme {
+        Box(modifier = Modifier.padding(20.dp)) {
+            ProfileBox()
+        }
+    }
+}
