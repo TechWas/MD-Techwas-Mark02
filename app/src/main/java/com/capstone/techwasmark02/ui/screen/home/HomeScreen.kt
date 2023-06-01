52c097614557f9a5f1b9807e223e3bcc7f611c5b
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-06-01 23:28:07 +0700

    modif: re-make home screen

--- app/src/main/java/com/capstone/techwasmark02/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/capstone/techwasmark02/ui/screen/home/HomeScreen.kt
@@ -1,15 +1,20 @@
 package com.capstone.techwasmark02.ui.screen.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,22 +23,40 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.util.lerp
+import coil.compose.rememberAsyncImagePainter
+import com.capstone.techwasmark02.R
 import com.capstone.techwasmark02.ui.component.ArticleCardBig
 import com.capstone.techwasmark02.ui.component.DefaultBottomBar
+import com.capstone.techwasmark02.ui.component.DetectBox1
+import com.capstone.techwasmark02.ui.component.DetectBox2
+import com.capstone.techwasmark02.ui.component.DetectBox3
+import com.capstone.techwasmark02.ui.component.DetectBox4
 import com.capstone.techwasmark02.ui.component.DetectionsFab
 import com.capstone.techwasmark02.ui.component.DropPointBanner
 import com.capstone.techwasmark02.ui.component.ForumBanner
@@ -41,7 +64,9 @@ import com.capstone.techwasmark02.ui.component.SelectableText
 import com.capstone.techwasmark02.ui.component.UserGreet
 import com.capstone.techwasmark02.ui.componentType.BottomBarItemType
 import com.capstone.techwasmark02.ui.theme.TechwasMark02Theme
+import com.capstone.techwasmark02.ui.theme.yellow
 import kotlin.math.absoluteValue
+import kotlin.random.Random
 
 @Composable
 fun HomeScreen() {
@@ -51,87 +76,144 @@ fun HomeScreen() {
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun HomeContent() {
-
-    val filterTypeList = listOf(
-        "General",
-        "Laptop",
-        "Baterai",
-        "Kabel",
-        "General",
-        "Laptop",
-        "Baterai",
-        "Kabel",
-        "General",
-        "Laptop",
-        "Baterai",
-        "Kabel",
-        "General",
-        "Laptop",
-        "Baterai",
-        "Kabel",
-    )
-
     Scaffold(
-
+        bottomBar = {
+            DefaultBottomBar(
+                selectedType = BottomBarItemType.Home
+            )
+        }
     ) { innerPadding ->
-        Box(
+        val scrollState = rememberScrollState()
+
+        Column(
             modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-                .padding(innerPadding),
-            contentAlignment = Alignment.BottomCenter
+                .verticalScroll(scrollState)
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.primary)
         ) {
-            Column(
+            Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
-                    .padding(bottom = 60.dp, top = 20.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .fillMaxWidth()
+                    .padding(top = 20.dp)
             ) {
-                Column(
+                Row(
                     modifier = Modifier
-                        .padding(horizontal = 16.dp)
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.Bottom
                 ) {
-                    UserGreet(userName = "Zhahrany")
+                    Image(
+                        modifier = Modifier.size(24.dp, 27.dp),
+                        painter = painterResource(id = R.drawable.logo_techwaste),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = "Techwas",
+                        modifier = Modifier
+                            .padding(start = 4.dp)
+                            .offset(y = 5.dp),
+                        style = MaterialTheme.typography.labelLarge,
+                        color = Color.White
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    IconButton(
+                        onClick = { },
+                        modifier = Modifier.size(21.dp, 24.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_nofitications),
+                            contentDescription = null,
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
 
-                    Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
 
-                    DropPointBanner()
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = 20.dp,
+                            topEnd = 20.dp
+                        )
+                    )
+                    .background(Color.White)
+                    .height(928.dp)
+            ) {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 18.dp)
+                    ) {
+                        Text(
+                            text = "Features",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        DetectBox1()
+                        DetectBox2()
+                    }
 
                     Spacer(modifier = Modifier.height(20.dp))
 
-                    Text(
-                        text = "What's New?",
-                        style = MaterialTheme.typography.labelLarge,
-                        modifier = Modifier.padding(start = 8.dp)
-                    )
-
-                    LazyRow(
-                        horizontalArrangement = Arrangement.Start,
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
                     ) {
-                        items(
-                            items = filterTypeList,
-                        ) { item ->
-                            SelectableText(
-                                text = item,
-                                selected = false
-                            )
-                        }
+                        DetectBox3()
+                        DetectBox4()
                     }
-                }
 
-                Spacer(modifier = Modifier.height(20.dp))
+                    Spacer(modifier = Modifier.height(10.dp))
 
-                val pagerState = rememberPagerState()
-                HorizontalPager(
-                    pageCount = 10,
-                    state = pagerState,
-                    contentPadding = PaddingValues(horizontal = 70.dp),
-                    modifier = Modifier.fillMaxWidth()
-                ) { page ->
-                    ArticleCardBig(
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 18.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Articles",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold
+                        )
+
+                        Spacer(modifier = Modifier.weight(1f))
+
+                        Text(
+                            text = "See all",
+                            style = MaterialTheme.typography.labelMedium,
+                            fontWeight = FontWeight.Bold,
+                            color = yellow
+                        )
+                    }
+
+                    val pagerState = rememberPagerState()
+                    HorizontalPager(
+                        pageCount = 10,
+                        state = pagerState,
+                        contentPadding = PaddingValues(horizontal = 70.dp),
+                        modifier = Modifier.fillMaxWidth()
+                    ) { page ->
+                        ArticleCardBig(
                             modifier = Modifier
-                                .width(240.dp)
+                                .width(272.dp)
+                                .height(139.dp)
                                 .graphicsLayer {
                                     // Calculate the absolute offset for the current page from the
                                     // scroll position. We use the absolute value which allows us to mirror
@@ -156,25 +238,46 @@ fun HomeContent() {
                                     )
                                 }
                         )
-                }
+                    }
 
-                Spacer(modifier = Modifier.height(20.dp))
+                    Spacer(modifier = Modifier.height(10.dp))
 
-                Column(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 18.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Forums",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold
+                        )
 
-                    ForumBanner()
+                        Spacer(modifier = Modifier.weight(1f))
 
+                        Text(
+                            text = "See all",
+                            style = MaterialTheme.typography.labelMedium,
+                            fontWeight = FontWeight.Bold,
+                            color = yellow
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 18.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        ArticleCardBig(
+                            modifier = Modifier
+                                .width(320.dp)
+                                .height(161.dp)
+                        )
+                    }
                 }
             }
-
-            DetectionsFab(
-                modifier = Modifier.padding(bottom = 25.dp)
-            )
-
-            DefaultBottomBar(selectedType = BottomBarItemType.Home)
         }
     }
 }
@@ -183,6 +286,6 @@ fun HomeContent() {
 @Composable
 fun HomeContentPreview() {
     TechwasMark02Theme {
-        HomeContent()
+        HomeScreen()
     }
 }
