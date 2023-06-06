eebe9251dcf1af65a6ba2f29cd0c22d5cec5e0d9
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-06-06 16:20:16 +0700

    modif: menambah id

--- app/src/main/java/com/capstone/techwasmark02/ui/componentType/ArticleFilterType.kt
+++ app/src/main/java/com/capstone/techwasmark02/ui/componentType/ArticleFilterType.kt
@@ -1,11 +1,11 @@
 package com.capstone.techwasmark02.ui.componentType
 
-sealed class ArticleFilterType(val type: String) {
+sealed class ArticleFilterType(val type: String, val id: Int) {
 
-    object General: ArticleFilterType(type = "General")
+    object General: ArticleFilterType(type = "General", id = 0)
 
-    object Laptop: ArticleFilterType(type = "Laptop")
+    object Laptop: ArticleFilterType(type = "Laptop", id = 1)
 
-    object Battery: ArticleFilterType(type = "Battery")
+    object Battery: ArticleFilterType(type = "Battery", id = 2)
 
 }
