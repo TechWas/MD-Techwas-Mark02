01df5db62ff2397aba7f4d3dae72c2521aa2d297
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-06-06 19:05:35 +0700

    modif: menambah endpoint article by component id

--- app/src/main/java/com/capstone/techwasmark02/data/remote/apiService/TechwasArticleApi.kt
+++ app/src/main/java/com/capstone/techwasmark02/data/remote/apiService/TechwasArticleApi.kt
@@ -3,12 +3,20 @@ package com.capstone.techwasmark02.data.remote.apiService
 import com.capstone.techwasmark02.data.remote.response.ArticleResultResponse
 import com.capstone.techwasmark02.data.remote.response.SingleArticleResponse
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TechwasArticleApi {
 
     @GET("/allArticle")
     suspend fun getAllArticle(): ArticleResultResponse
+
+    @GET("/article/{id}")
+    suspend fun getArticleByComponentId(
+        @Header("Authorization") token: String,
+        @Query("compid") compid: Int
+    ): ArticleResultResponse
 
     @GET("/article/id/{id}")
     suspend fun getArticleById(
