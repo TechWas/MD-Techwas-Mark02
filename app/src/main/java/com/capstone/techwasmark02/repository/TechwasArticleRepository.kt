ce028fbc1b942ade1e5b1cec82eaa3ee71d3912c
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-06-06 16:19:30 +0700

    modif: menambah method fetch article

--- app/src/main/java/com/capstone/techwasmark02/repository/TechwasArticleRepository.kt
+++ app/src/main/java/com/capstone/techwasmark02/repository/TechwasArticleRepository.kt
@@ -2,6 +2,7 @@ package com.capstone.techwasmark02.repository
 
 import com.capstone.techwasmark02.data.remote.apiService.TechwasArticleApi
 import com.capstone.techwasmark02.data.remote.response.ArticleResultResponse
+import com.capstone.techwasmark02.data.remote.response.SingleArticleResponse
 import com.capstone.techwasmark02.ui.common.UiState
 import javax.inject.Inject
 
@@ -11,6 +12,24 @@ class TechwasArticleRepository @Inject constructor(
     suspend fun getAllArticle(): UiState<ArticleResultResponse> {
         val response = try {
             articleApi.getAllArticle()
+        } catch (e: Exception) {
+            return UiState.Error(message = "fail to fetch article, ${e.message}")
+        }
+        return UiState.Success(data = response, message = "Success to fetch article")
+    }
+
+    suspend fun getArticleById(id: Int): UiState<SingleArticleResponse> {
+        val response = try {
+            articleApi.getArticleById(id)
+        } catch (e: Exception) {
+            return UiState.Error(message = "fail to fetch article, ${e.message}")
+        }
+        return UiState.Success(data = response, message = "Success to fetch article")
+    }
+
+    suspend fun getArticleByName(name: String): UiState<ArticleResultResponse> {
+        val response = try {
+            articleApi.getArticleByName(name)
         } catch (e: Exception) {
             return UiState.Error(message = "fail to fetch article, ${e.message}")
         }
