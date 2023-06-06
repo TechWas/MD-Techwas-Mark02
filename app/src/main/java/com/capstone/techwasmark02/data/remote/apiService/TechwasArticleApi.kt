883e4fa3bda50f38c581e340832a0eeb905a10c7
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-06-06 16:18:10 +0700

    modif: menambah api article

--- app/src/main/java/com/capstone/techwasmark02/data/remote/apiService/TechwasArticleApi.kt
+++ app/src/main/java/com/capstone/techwasmark02/data/remote/apiService/TechwasArticleApi.kt
@@ -1,13 +1,24 @@
 package com.capstone.techwasmark02.data.remote.apiService
 
 import com.capstone.techwasmark02.data.remote.response.ArticleResultResponse
-import retrofit2.Response
+import com.capstone.techwasmark02.data.remote.response.SingleArticleResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface TechwasArticleApi {
 
     @GET("/allArticle")
     suspend fun getAllArticle(): ArticleResultResponse
+
+    @GET("/article/id/{id}")
+    suspend fun getArticleById(
+        @Path("id") id: Int
+    ): SingleArticleResponse
+
+    @GET("/article/name/{name}")
+    suspend fun getArticleByName(
+        @Path("name") name: String
+    ): ArticleResultResponse
 
     companion object {
         const val BASE_URL = "https://the-prophecy-fwd5gpydiq-uc.a.run.app/"
