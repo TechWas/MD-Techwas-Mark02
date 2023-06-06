da24d034fbc31ade76a68297eb6f93a53d6d348a
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-06-06 19:07:43 +0700

    modif: menambah method getArticleByComponentId

--- app/src/main/java/com/capstone/techwasmark02/ui/screen/article/ArticleScreenViewModel.kt
+++ app/src/main/java/com/capstone/techwasmark02/ui/screen/article/ArticleScreenViewModel.kt
@@ -23,47 +23,17 @@ class ArticleScreenViewModel @Inject constructor(
 
     init {
        viewModelScope.launch {
-           getAllArticle()
+           getAllFilterArticle(0)
        }
-    }
-
-    fun getAllArticle() {
-        _articleList.value = UiState.Loading()
-        viewModelScope.launch {
-            val result = articleRepository.getAllArticle()
-            when(result) {
-                is UiState.Success -> {
-                    _articleList.value = result
-                }
-                is UiState.Error -> {
-                    _articleList.value = result
-                }
-                else -> {
-                    // do nothing
-                }
-            }
-        }
     }
 
     fun getAllFilterArticle(id: Int) {
         _articleList.value = UiState.Loading()
         viewModelScope.launch {
-            val result = articleRepository.getAllArticle()
+            val result = articleRepository.getArticleByComponentId(userToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySUQiOiJ1c2VyQGV4YW1wbGUuY29tIiwiZXhwaXJ5IjoxNjg2MDQ5NDcwLjc1NjgyMTR9.eJfMxHb3UsQu-kyzyzN_3PdV8OvvwTmD8vOyoTRENyQ'", id = id)
             when(result) {
                 is UiState.Success -> {
-                    if(id == 0) {
-                        _articleList.value = result
-                    } else {
-                        val filteredArticles = result.data?.componentList?.filter {
-                            it?.componentId == id
-                        }
-                        val filteredResult = ArticleResultResponse(
-                            componentList = filteredArticles.orEmpty(),
-                            error = result.message,
-                            message = result.message
-                        )
-                        _articleList.value = UiState.Success(filteredResult)
-                    }
+                    _articleList.value = result
                 }
                 is UiState.Error -> {
                     _articleList.value = result
@@ -74,24 +44,6 @@ class ArticleScreenViewModel @Inject constructor(
             }
         }
     }
-
-//    fun getArticleByName(name: String) {
-//        _articleList.value = UiState.Loading()
-//        viewModelScope.launch {
-//            val result = articleRepository.getArticleByName(name)
-//            when(result) {
-//                is UiState.Success -> {
-//                    _articleList.value = result
-//                }
-//                is UiState.Error -> {
-//                    _articleList.value = result
-//                }
-//                else -> {
-//                    // do nothing
-//                }
-//            }
-//        }
-//    }
 
     fun getArticleByName(name: String, filterType: ArticleFilterType) {
         _articleList.value = UiState.Loading()
@@ -99,20 +51,20 @@ class ArticleScreenViewModel @Inject constructor(
             if (name.isEmpty()) { // input kosong fetch article by filter
                 getAllFilterArticle(filterType.id)
             } else {
-                val result = articleRepository.getAllArticle()
+                val result = articleRepository.getArticleByComponentId(userToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySUQiOiJ1c2VyQGV4YW1wbGUuY29tIiwiZXhwaXJ5IjoxNjg2MDQ5NDcwLjc1NjgyMTR9.eJfMxHb3UsQu-kyzyzN_3PdV8OvvwTmD8vOyoTRENyQ'", id = filterType.id)
                 when(result) {
                     is UiState.Success -> {
                         val filteredArticles = if (filterType.id == 0) { // general
-                            result.data?.componentList?.filter {
+                            result.data?.articleList?.filter {
                                 it?.name?.contains(name, ignoreCase = true) == true
                             }
                         } else {
-                            result.data?.componentList?.filter {
+                            result.data?.articleList?.filter {
                                 it?.name?.contains(name, ignoreCase = true) == true && it.componentId == filterType.id
                             }
                         }
                         val filteredResult = ArticleResultResponse(
-                            componentList = filteredArticles.orEmpty(),
+                            articleList = filteredArticles.orEmpty(),
                             error = result.message,
                             message = result.message
                         )
