3d30e7c79958e3a66c7fe47348fc1769ac4067eb
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-06-08 18:19:55 +0700

    modif: adjust SelectableText

--- app/src/main/java/com/capstone/techwasmark02/ui/screen/article/ArticleScreen.kt
+++ app/src/main/java/com/capstone/techwasmark02/ui/screen/article/ArticleScreen.kt
@@ -1,6 +1,7 @@
 package com.capstone.techwasmark02.ui.screen.article
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidthIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -20,8 +22,10 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,6 +36,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -146,18 +151,29 @@ fun ArticleContent(
             Spacer(modifier = Modifier.height(16.dp))
 
             LazyRow(
+                modifier = Modifier.height(48.dp),
                 horizontalArrangement = Arrangement.spacedBy(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 items(
                     items = filterTypeList,
                 ) { item ->
-                    SelectableText(
-                        filterType = item,
-                        selected = item == selectedFilter,
-                        modifier = Modifier.clickable(enabled = item != selectedFilter) {
+                    IconButton(
+                        modifier = Modifier.requiredWidthIn(min = 60.dp),
+                        onClick = {
                             selectedFilter = item
                             viewModel.getAllFilterArticle(item.id)
+                        },
+                        enabled = item != selectedFilter,
+                        content = {
+                            SelectableText(
+                                filterType = item,
+                                selected = item == selectedFilter,
+//                                modifier = Modifier.clickable(enabled = item != selectedFilter) {
+//                                    selectedFilter = item
+//                                    viewModel.getAllFilterArticle(item.id)
+//                                }
+                            )
                         }
                     )
                 }
@@ -207,11 +223,25 @@ fun ArticleContent(
                             Box(
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .align(Alignment.CenterHorizontally)
+                                    .height(100.dp),
+                                contentAlignment = Alignment.Center
                             ) {
-                                Text(
-                                    text = "Cari apa tuh kira-kira"
-                                )
+                                Box(
+                                    modifier = Modifier
+                                        .border(
+                                            width = 1.dp,
+                                            color = Color.Red,
+                                            shape = RoundedCornerShape(20.dp)
+                                        )
+                                        .padding(8.dp)
+
+                                ) {
+                                    Text(
+                                        text = "There's no related article",
+                                        style = MaterialTheme.typography.labelSmall,
+                                        color = Color.Red
+                                    )
+                                }
                             }
                         }
 
