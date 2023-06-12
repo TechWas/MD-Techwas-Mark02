335f36e2381a86ba632c794ee71dd1c5c30594dc
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-06-12 23:05:56 +0700

    change splashscreen

--- app/src/main/java/com/capstone/techwasmark02/ui/screen/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/capstone/techwasmark02/ui/screen/splashScreen/SplashScreen.kt
@@ -1,32 +1,33 @@
 package com.capstone.techwasmark02.ui.screen.splashScreen
 
-import android.window.SplashScreen
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.*
-import androidx.compose.ui.draw.alpha
 import com.capstone.techwasmark02.R
 import com.capstone.techwasmark02.ui.theme.Green77
 import com.capstone.techwasmark02.ui.theme.TechwasMark02Theme
@@ -59,19 +60,46 @@ fun SplashScreen() {
 fun SplashContent(alpha: Float) {
     Box(
         modifier = Modifier
-            .background(
-                Green77
-            )
-            .fillMaxSize(),
+            .fillMaxSize()
+            .alpha(alpha)
+            .background(color = Green77),
         contentAlignment = Alignment.Center
     ) {
-
-        Image(
-            painter = painterResource(id = R.drawable.logo_techwase),
-            contentDescription = "logo techwaste",
+        Row(
             modifier = Modifier
-                .size(100.dp)
-                .alpha(alpha)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_techwaste),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(57.27.dp)
+                    .height(63.9.dp)
+            )
+            Text(
+                text = "Techwaste",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(start = 5.24.dp)
+            )
+        }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Text(
+            text = "Copyright © 2023",
+            color= Color.White,
+            style = MaterialTheme.typography.labelMedium,
+            modifier = Modifier
+                .padding(bottom = 32.dp)
         )
     }
 }
